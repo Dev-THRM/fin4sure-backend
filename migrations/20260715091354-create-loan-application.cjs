615b3f29b78c8a40850084a37b6f12612f48bd5e
Sneha Dewani <snehadewani07@gmail.com>
2026-07-16 11:11:33 +0530

    created loan application migration and model

---
+++ migrations/20260715091354-create-loan-application.cjs
@@ -0,0 +1,49 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Loan_Applications', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      application_no: {
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER
+      },
+      lender_id: {
+        type: Sequelize.INTEGER
+      },
+      loan_type_id: {
+        type: Sequelize.INTEGER
+      },
+      loan_amount: {
+        type: Sequelize.DOUBLE
+      },
+      loan_purpose: {
+        type: Sequelize.STRING
+      },
+      tenure: {
+        type: Sequelize.INTEGER
+      },
+      status_id: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Loan_Applications');
+  }
+};
