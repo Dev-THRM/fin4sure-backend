22ff152431cb0870e98f8f45264283e6f477be5a
Sneha Dewani <snehadewani07@gmail.com>
2026-07-17 11:50:28 +0530

    created models for lender and loan applications

---
+++ models/lender_application.js
@@ -0,0 +1,22 @@
+import { DataTypes } from 'sequelize';
+import { sequelize } from '../config/db.js';
+
+const Lender_Application = sequelize.define('Lender_Application', {
+  loan_application_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  lender_rate_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  status: {
+    type: DataTypes.ENUM('active', 'pending', 'inactive'),
+    allowNull: false,
+    defaultValue: 'pending'
+  }
+}, {
+  timestamps: true,
+});
+
+export default Lender_Application;
--- models/loan_application.js
+++ models/loan_application.js
@@ -4,15 +4,12 @@ import { sequelize } from '../config/db.js';
 const Loan_Application = sequelize.define('Loan_Application', {
   application_no: DataTypes.INTEGER,
   user_id: DataTypes.INTEGER,
-  lender_id: DataTypes.INTEGER,
   loan_type_id: DataTypes.INTEGER,
   loan_amount: DataTypes.DOUBLE,
   loan_purpose: DataTypes.STRING,
   tenure: DataTypes.INTEGER,
   status_id: DataTypes.INTEGER,
-  min_rate: DataTypes.DOUBLE,
-  max_rate: DataTypes.DOUBLE,
-  rate: DataTypes.DOUBLE
+  partner_id: DataTypes.INTEGER
 }, {
   timestamps: true,
 });
