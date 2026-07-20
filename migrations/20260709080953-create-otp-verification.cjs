14f68d1cc7fe422d0d2f66c425eba298e01c6677
Sneha Dewani <snehadewani07@gmail.com>
2026-07-20 18:02:14 +0530

    fix: db lowercase

--- migrations/20260709080953-create-otp-verification.cjs
+++ migrations/20260709080953-create-otp-verification.cjs
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Otp_Verifications', {
+    await queryInterface.createTable('otp_verifications', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -38,6 +38,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Otp_Verifications');
+    await queryInterface.dropTable('otp_verifications');
   }
 };
