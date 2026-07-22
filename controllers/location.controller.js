1cc9ff6c7f271aaa296e6beda15af581e236fcc7
Sneha Dewani <snehadewani07@gmail.com>
2026-07-22 13:29:12 +0530

    automatic fetching of location based on pincode

--- controllers/location.controller.js
+++ controllers/location.controller.js
@@ -1,6 +1,7 @@
 import State from "../models/state.js";
 import District from "../models/district.js";
 import City from "../models/city.js";
+import Pincode from "../models/pincode.js";
 
 export const getStates = async (req, res) => {
   try {
@@ -26,6 +27,45 @@ export const getCities = async (req, res) => {
     const { districtId } = req.params;
     const cities = await City.findAll({ where: { district_id: districtId }, order: [['name', 'ASC']] });
     res.status(200).json({ success: true, data: cities });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+export const getLocationByPincode = async (req, res) => {
+  try {
+    const { pincode } = req.params;
+    const pincodeRecord = await Pincode.findOne({
+      where: { code: pincode },
+      include: [
+        {
+          model: City,
+          include: [
+            {
+              model: District,
+              include: [State]
+            }
+          ]
+        }
+      ]
+    });
+
+    if (!pincodeRecord) {
+      return res.status(404).json({ success: false, message: 'Pincode not found' });
+    }
+
+    const city = pincodeRecord.City;
+    const district = city.District;
+    const state = district.State;
+
+    res.status(200).json({
+      success: true,
+      data: {
+        city: { id: city.id, name: city.name },
+        district: { id: district.id, name: district.name },
+        state: { id: state.id, name: state.name }
+      }
+    });
   } catch (error) {
     res.status(500).json({ success: false, message: error.message });
   }
--- routes/location.routes.js
+++ routes/location.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getStates, getDistricts, getCities } from "../controllers/location.controller.js";
+import { getStates, getDistricts, getCities, getLocationByPincode } from "../controllers/location.controller.js";
 
 const router = express.Router();
 
 router.get("/states", getStates);
 router.get("/districts/:stateId", getDistricts);
 router.get("/cities/:districtId", getCities);
+router.get("/pincode/:pincode", getLocationByPincode);
 
 export default router;
