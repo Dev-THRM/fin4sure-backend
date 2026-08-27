f95a59f05587e970662165ab7226b5196a119bc5
Sahil Bijlani <bijlanisahil511@gmail.com>
2026-08-27 17:54:59 +0530

    fix: add /api/lenders/rates public endpoint returning full 45+ banks rates matching admin configuration

--- routes/lender.routes.js
+++ routes/lender.routes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getLenders } from '../controllers/lender.controller.js';
+import { getLenders, getPublicLenderRates } from '../controllers/lender.controller.js';
 
 const router = express.Router();
 
 router.get('/', getLenders);
+router.get('/rates', getPublicLenderRates);
 
 export default router;
