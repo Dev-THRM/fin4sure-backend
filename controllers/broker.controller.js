030b264b5c3ff33f5c8b1ded1816fc45f9674858
Sneha Dewani <snehadewani07@gmail.com>
2026-09-11 02:39:24 +0530

    indentation error

--- controllers/broker.controller.js
+++ controllers/broker.controller.js
@@ -487,7 +487,7 @@ export const referClient = async (req, res) => {
       //   console.error("WhatsApp message failed:", waError?.response?.data || waError.message);
       // }
       // --- END old WhatsApp simulation ---
-    }
+    // }
 
     return res.status(201).json({
       success: true,
